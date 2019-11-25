141b26f83c77bf43272104734d272debb5e01392
thor56 <38258444+thor56@users.noreply.github.com>
2019-11-25 18:35:15 +0530

    Update header.php

--- header.php
+++ header.php
@@ -14,7 +14,7 @@ session_start();
     <link rel="stylesheet" href="style1.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     
-    <title>Hello, world!</title>
+    <title>Forum</title>
   
                                     <!--    CONTENT   -->
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark" >
